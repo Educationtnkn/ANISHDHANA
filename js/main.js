4a3748c56d382dc03afa56526806ba5fe271bd33
P,ANISH KUMAR <98410447+AnishKumarPandian@users.noreply.github.com>
2022-01-27 12:52:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(window).on('load', function() {
 		Preloder
 	--------------------*/
 	$(".loader").fadeOut();
-	$("#preloder").delay(100).fadeOut("slow");
+	$("#preloder").fadeOut();
 
 	/*------------------
 		Masonry
